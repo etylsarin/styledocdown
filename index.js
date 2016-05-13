3ba99f88cd65757782d61e4f8c2cf860d505dd91
Filip Mares <maresf@gmail.com>
2016-05-13 09:13:18 +0200

    support for tutorial added
    code cleanup, library split to improve maintainability

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-module.exports = require('./lib/styledocdown');
+var docparse = require('./lib/docparse'),
+docdown = require('./lib/docdown');
+
+function sync (text, options) {
+    text = typeof text === 'string' ? text : null;
+    options = typeof options === 'object' ? options : {};
+    options.root = options.root || process.cwd();
+    options.fileName = options.fileName || 'style.css';
+
+    if (text) {
+        return docdown(docparse(text), options);
+    } else {
+        return new Error('styledocdown: First arg must be a string.');
+    }
+};
+
+module.exports = function (text, options) {
+    var lastArgument = arguments[arguments.length - 1],
+    cb = typeof lastArgument === 'function' ? lastArgument : null,
+    data = sync(text, options);
+
+    if (cb) {
+        if (data instanceof Error) {
+            cb(data);
+        } else {
+            cb(null, data);
+        }
+    }
+};
+
+module.exports.sync = sync;
